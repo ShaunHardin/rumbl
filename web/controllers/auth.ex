e6538021fdc497ee0e8f8100c3fa3a708b372684
Shaun Hardin <shardin@covermymeds.com>
2017-02-13 21:14:21 -0500

    Add annotations, set up annotations via channels

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -16,9 +16,9 @@ defmodule Rumbl.Auth do
 
     cond do
       user = conn.assigns[:current_user] ->
-        conn
+        put_current_user(conn, user)
       user = user_id && repo.get(Rumbl.User, user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
       true ->
         assign(conn, :current_user, nil)
     end
@@ -26,7 +26,7 @@ defmodule Rumbl.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
@@ -59,5 +59,13 @@ defmodule Rumbl.Auth do
       |> redirect(to: Helpers.page_path(conn, :index))
       |> halt()
     end
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 end
--- web/static/js/video.js
+++ web/static/js/video.js
@@ -15,11 +15,81 @@ let Video = {
     let msgInput = document.getElementById("msg-input")
     let postButton = document.getElementById("msg-submit")
     let vidChannel = socket.channel("videos:" + videoId)
-    vidChannel.on("ping", ({count}) => console.log("PING", count))
+
+    postButton.addEventListener("click", e => {
+      let payload = {body: msgInput.value, at: Player.getCurrentTime()}
+      vidChannel.push("new_annotation", payload)
+        .receive("error", error => console.log(error))
+      msgInput.value = ""
+    })
+
+    vidChannel.on("new_annotation", (resp) => {
+      vidChannel.params.last_seen_id = resp.id
+      this.renderAnnotation(msgContainer, resp)
+    })
+
+    msgContainer.addEventListener("click", e => {
+      e.preventDefault()
+      let seconds = e.target.getAttribute("data-seek") ||
+        e.target.parentNode.getAttribute("data-seek")
+
+      if(!seconds) { return }
+
+      Player.seekTo(seconds)
+    })
 
     vidChannel.join()
-      .receive("ok", resp => console.log("joined the video channel", resp))
+      .receive("ok", resp => {
+        let ids = resp.annotations.map(ann => ann.id)
+        if (ids.length > 0) { vidChannel.params.last_seen_id = Math.max(...ids)}
+        this.scheduleMessages(msgContainer, resp.annotations)
+      })
       .receive("error", reason => console.log("join failed", reason))
+  },
+
+  esc(str) {
+    let div = document.createElement("div")
+    div.appendChild(document.createTextNode(str))
+    return div.innerHTML
+  },
+
+  renderAnnotation(msgContainer, {user, body, at}) {
+    let template = document.createElement("div")
+
+    template.innerHTML = `
+    <a href="#" data-seek="${this.esc(at)}">
+      [${this.formatTime(at)}]
+      <b>${this.esc(user.username)}</b>: ${this.esc(body)}
+    </a>
+    `
+    msgContainer.appendChild(template)
+    msgContainer.scrollTop = msgContainer.scrollHeight
+  },
+
+  scheduleMessages(msgContainer, annotations) {
+    setTimeout(() => {
+      let currentTime = Player.getCurrentTime()
+      let remaining = this.renderAtTime(annotations, currentTime, msgContainer)
+      this.scheduleMessages(msgContainer, remaining)
+    }, 1000)
+  },
+
+  renderAtTime(annotations, seconds, msgContainer) {
+    return annotations.filter(ann => {
+      if (ann.at > seconds) {
+        return true
+      }
+      else {
+        this.renderAnnotation(msgContainer, ann)
+        return false
+      }
+    })
+  },
+
+  formatTime(at) {
+    let date = new Date(null)
+    date.setSeconds(at / 1000)
+    return date.toISOString().substr(14, 5)
   }
 }
 export default Video
